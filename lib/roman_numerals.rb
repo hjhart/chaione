eedb74861e0ce7f340dcb82bcafa6991f9927f48
James Hart <hjhart@gmail.com>
2013-06-15 14:01:06 -0700

    Roman numerals 10 through 50.

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,26 +1,35 @@
 class RomanNumerals
-  def self.convert(number)
+  def self.convert(remainder)
     roman_numeral = ''
-    remainder = number
-    if(remainder == 10)
-      roman_numeral += 'X'
-      remainder = remainder - 10
-    end
-    if(remainder == 9)
-      roman_numeral += 'IX'
-      remainder = remainder - 9
-    end
-    if(remainder <= 8 && remainder >= 5)
-      roman_numeral += 'V'
-      remainder = remainder - 5
-    end
-    if(remainder == 4)
-      roman_numeral += 'IV'
-      remainder = remainder - 4
-    end
-    if(remainder <= 3 && remainder != 0)
-      roman_numeral += 'I' * remainder
-    end
+    begin
+      remainder, roman_numeral = reduce(remainder, roman_numeral)
+    end while remainder > 0
     roman_numeral
   end
+
+  private
+
+  def self.reduce(remainder, roman_numeral)
+    if (remainder >= 50)
+      roman_numeral += 'L'
+      remainder = remainder - 50
+    elsif (remainder >= 10)
+      roman_numeral += 'X'
+      remainder = remainder - 10
+    elsif (remainder == 9)
+      roman_numeral += 'IX'
+      remainder = remainder - 9
+    elsif (remainder <= 8 && remainder >= 5)
+      roman_numeral += 'V'
+      remainder = remainder - 5
+    elsif (remainder == 4)
+      roman_numeral += 'IV'
+      remainder = remainder - 4
+    elsif (remainder <= 3)
+      roman_numeral += 'I' * remainder
+      remainder = 0
+    end
+    [remainder, roman_numeral]
+  end
 end
+
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -26,7 +26,33 @@ describe RomanNumerals do
     RomanNumerals.convert(9).should eq 'IX'
   end
 
-  it 'should convert 5 to V' do
+  it 'should convert 10 to X' do
     RomanNumerals.convert(10).should eq 'X'
+  end
+
+  it 'should convert 11, 12, 13 to XI, XII, XIII' do
+    RomanNumerals.convert(11).should eq 'XI'
+    RomanNumerals.convert(12).should eq 'XII'
+    RomanNumerals.convert(13).should eq 'XIII'
+  end
+
+  it 'should convert 14 to XIV' do
+    RomanNumerals.convert(14).should eq 'XIV'
+  end
+
+  it 'should convert 19 to XIX' do
+    RomanNumerals.convert(19).should eq 'XIX'
+  end
+
+  it 'should convert 21 to XXI' do
+    RomanNumerals.convert(21).should eq 'XXI'
+  end
+
+  it 'should convert 29 to XXIX' do
+    RomanNumerals.convert(29).should eq 'XXIX'
+  end
+
+  it 'should convert 50 to L' do
+    RomanNumerals.convert(50).should eq 'L'
   end
 end
