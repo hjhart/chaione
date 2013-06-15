2245cc06df8bf1415293d732dd3edc0ec9a745c0
James Hart <hjhart@gmail.com>
2013-06-15 13:43:43 -0700

    Roman numerals 6 through 10

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -2,16 +2,25 @@ class RomanNumerals
   def self.convert(number)
     roman_numeral = ''
     remainder = number
-    if(remainder == 5)
+    if(remainder == 10)
+      roman_numeral += 'X'
+      remainder = remainder - 10
+    end
+    if(remainder == 9)
+      roman_numeral += 'IX'
+      remainder = remainder - 9
+    end
+    if(remainder <= 8 && remainder >= 5)
       roman_numeral += 'V'
-      remainder = number - 5
+      remainder = remainder - 5
     end
     if(remainder == 4)
       roman_numeral += 'IV'
-      remainder = number - 4
+      remainder = remainder - 4
     end
-    if(remainder <= 3)
+    if(remainder <= 3 && remainder != 0)
       roman_numeral += 'I' * remainder
     end
+    roman_numeral
   end
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -15,4 +15,18 @@ describe RomanNumerals do
   it 'should convert 5 to V' do
     RomanNumerals.convert(5).should eq 'V'
   end
+
+  it 'should convert 6 through 8 similarly' do
+    RomanNumerals.convert(6).should eq 'VI'
+    RomanNumerals.convert(7).should eq 'VII'
+    RomanNumerals.convert(8).should eq 'VIII'
+  end
+
+  it 'should convert 9 to IX' do
+    RomanNumerals.convert(9).should eq 'IX'
+  end
+
+  it 'should convert 5 to V' do
+    RomanNumerals.convert(10).should eq 'X'
+  end
 end
